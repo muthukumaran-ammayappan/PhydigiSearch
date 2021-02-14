aff3d21823d83ce1b3880e483eed9a8e773d5e05
Murugesan <murugesan.madasaminambi@impigertech.com>
2021-02-14 11:01:42 +0530

    images, get lat,long

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -34,10 +34,9 @@ export class SearchComponent implements OnInit {
   img;
 
 
-  public bengalurLat = '11.080024389224244';
-  public bengalurLng = '76.9405519018713';
-  // public bengalurLat = '13.066412600498435';
-  // public bengalurLng = '77.60393005906081';
+
+  public bengalurLat = '13.066412600498435';
+  public bengalurLng = '77.60393005906081';
   public lat = null;
   public lng = null;
 
